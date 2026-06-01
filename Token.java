bc3a67b27f4e11c9cca8e746da427c1dbe8ab948
Khushi Pandey <khushipan1805@gmail.com>
2026-06-01 17:47:10 +0000

    Defined the constituents of a token

---
+++ Token.java
@@ -0,0 +1,20 @@
+package com.github.khushipandey1805.bobo;
+
+class Token{
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    //constructor
+    Token(TokenType type, String lexeme, Object literal, int line){
+        this.type=type;
+        this.lexeme=lexeme;
+        this.literal=literal;
+        this.line=line;
+    }
+
+    public String toString(){
+        return type+" "+lexeme+" "+literal;
+    }
+}
