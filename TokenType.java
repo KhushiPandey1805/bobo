eb588d0ddeefccea2316acc2c43725bb5365b9a1
Khushi Pandey <khushipan1805@gmail.com>
2026-06-01 18:22:09 +0000

    Added the hash symbol as a comment starter instead of double slash

--- TokenType.java
+++ TokenType.java
@@ -4,7 +4,7 @@ enum TokenType{
     //single character tokens
     LEFT_PAREN, RIGHT_PAREN, 
     LEFT_BRACE, RIGHT_BRACE, 
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, 
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, HASH,
 
     //one or two character tokens
     BANG, BANG_EQUAL,
