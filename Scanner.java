06eb08bc2323a5e2ae8e110d1db69aa9da95c358
Khushi Pandey <khushipan1805@gmail.com>
2026-06-02 17:39:02 +0000

    Added mostly everything except reserved words and identifiers

--- Scanner.java
+++ Scanner.java
@@ -51,11 +51,43 @@ class Scanner{
             case '\r': //ignore carriage return
             case '\t': break; //ignore tab
             case '\n': line++; break;
-            default: Bobo.error(line, "Unexpected character."); break;
+            case '"': string(); break;
+            default: 
+                if(isDigit(c)) //any digit
+                    number();
+                else 
+                    Bobo.error(line, "Unexpected character."); 
+                break;
         }
     }
 
-    private boolean match(char expected){
+    private void number(){
+        while(isDigit(peek()))
+            advance();
+        if(peek()=='.' && isDigit(peekNext())){ // we dont support 123., currently dont support .123
+            advance();
+            while(isDigit(peek()))
+                advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.susbstring(start,current))); //future notes: do the parsing ourselves instead of using java parsing methods
+    }
+
+    private void string(){ //escape characters not supported atp, will add later maybe
+        while(peek()!='"' && !isAtEnd()){
+            if(peek()=='\n') //multi line strings allowed :o (because easier lmao, might change later)
+                line++;
+            advance();
+        }
+        if(!isAtEnd()){
+            Bobo.error(line, "Unterminated string");
+            return;
+        }
+        advance(); //to "eat up" the closing "
+        String value=source.substring(start+1, current-1); //only the text inside double quotes
+        addToken(STRING, value);
+    }
+
+    private boolean match(char expected){ //is next character what i want it to be?
         if(isAtEnd()) return false;
         if(source.charAt(current)!=expected)
             return false;
@@ -63,17 +95,29 @@ class Scanner{
         return true;
     }
 
-    private char peek(){
+    private char peek(){ //immediate next character, just peeking no advancing
         if(isAtEnd())
             return '\0';
         return source.charAt(current);
     }
 
-    private boolean isAtEnd(){
+    private char peekNext(){ //the character after the next one; again, no advancing
+        if(current+1>=source.length())
+            return '\0';
+        return source.charAt(current+1);
+    }
+
+    private boolean isDigit(char c){ //overriding the standard function isDigit(), to reduce complexities of unwanted digits
+        if(c>='0' && c<='9')
+            return true;
+        return false;
+    }
+
+    private boolean isAtEnd(){ //at the eof?
         return current>=source.length();
     }
 
-    private char advance(){
+    private char advance(){ //eat up the characters, move on
         return source.charAt(current++);
     }
     
