72fc75d3f3894374630d8eacc5493a52900dcdfa
Khushi Pandey <khushipan1805@gmail.com>
2026-06-22 17:57:53 +0000

    Added Runtime error handling

--- Interpreter.java
+++ Interpreter.java
@@ -1,6 +1,14 @@
 package com.github.khushipandey1805.bobo;
 
 class Interpreter implements Expr.Visitor<Object>{
+    void interpret(Expr expression){
+        try{
+            Object value=evaluate(expression);
+            System.out.println(stringify(value));
+        } catch(RuntimeError error){
+            Bobo.runtimeError(error);
+        }
+    }
     @Override
     public Object visitLiteralExpr(Expr.Literal expr){
         return expr.value;
@@ -10,11 +18,22 @@ class Interpreter implements Expr.Visitor<Object>{
         Object right=evaluate(expr.right);
         switch(expr.operator.type){
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
             case BANG:
                 return !isTruthy(right);
         }
         return null;
+    }
+    private void checkNumberOperand(Token operator, Object operand){
+        if(operand instanceof Double)
+            return;
+        throw new RuntimeError(operator, "Operand must be a number. Can't have a negative cow, yk. Unless you can??");
+    }
+    private void checkNumberOperands(Token operator, Object left, Object right){
+        if(left instanceof Double && right instanceof Double)
+            return;
+        throw new RuntimeError(operator, "Operators must be numbers. Can't compare apples and oranges afaik.");
     }
     private boolean isTruthy(Object object){
         if(object==null) //null is falsey
@@ -30,6 +49,18 @@ class Interpreter implements Expr.Visitor<Object>{
             return false;
         return a.equals(b);
     }
+    private String stringify(Object object){
+        if(object==null)
+            return "nil";
+        if(object instanceof Double){
+            String text=object.toString();
+            if(text.endsWith(".0")){
+                text=text.substring(0, text.length()-2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr){
         return evaluate(expr.expression);
@@ -43,18 +74,23 @@ class Interpreter implements Expr.Visitor<Object>{
         Object right=evaluate(expr.right);
         switch(expr.operator.type){
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left>(double)right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left>=(double)right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left<(double)right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left<=(double)right;
             case BANG_EQUAL:
                 return !isEqual(left, right);
             case EQUAL_EQUAL:
                 return isEqual(left, right);
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left-(double)right;
             case PLUS:
                 if(left instanceof Double && right instanceof Double){
@@ -63,10 +99,12 @@ class Interpreter implements Expr.Visitor<Object>{
                 if(left instanceof String && right instanceof String){
                     return (String)left+(String)right;
                 }
-                break;
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings. Is that not enough for you >:(");
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left/(double)right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left*(double)right;
         }
         return null;
