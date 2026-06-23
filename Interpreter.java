9d7efde562ea0701243746c12941ac8a1bd9dc18
Khushi Pandey <khushipan1805@gmail.com>
2026-06-23 18:42:46 +0000

    Added statement interpreting

--- Interpreter.java
+++ Interpreter.java
@@ -1,10 +1,13 @@
 package com.github.khushipandey1805.bobo;
 
-class Interpreter implements Expr.Visitor<Object>{
-    void interpret(Expr expression){
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void>{
+    void interpret(List<Stmt> statements){
         try{
-            Object value=evaluate(expression);
-            System.out.println(stringify(value));
+            for(Stmt statement:statements){
+                execute(statement);
+            }
         } catch(RuntimeError error){
             Bobo.runtimeError(error);
         }
@@ -67,6 +70,20 @@ class Interpreter implements Expr.Visitor<Object>{
     }
     private Object evaluate(Expr expr){
         return expr.accept(this);
+    }
+    private void execute(Stmt stmt){
+        stmt.accept(this);
+    }
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt){
+        evaluate(stmt.expression);
+        return null;
+    }
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt){
+        Object value=evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
     @Override
     public Object visitBinaryExpr(Expr.Binary expr){
