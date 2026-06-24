6288ec60a33b51de1d48ce981f831aea0c973130
Khushi Pandey <khushipan1805@gmail.com>
2026-06-24 19:50:33 +0000

    Added variable expression handling

--- Expr.java
+++ Expr.java
@@ -3,14 +3,13 @@ package com.github.khushipandey1805.bobo;
 import java.util.List;
 
 abstract class Expr {
-
     interface Visitor<R>{
         R visitBinaryExpr(Binary expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
         R visitUnaryExpr(Unary expr);
+        R visitVariableExpr(Variable expr);
     }
-
     static class Binary extends Expr {
         Binary(Expr left, Token operator, Expr right){
             this.left=left;
@@ -25,7 +24,6 @@ abstract class Expr {
         final Token operator;
         final Expr right;
     }
-
     static class Grouping extends Expr {
         Grouping(Expr expression){
             this.expression=expression;
@@ -36,7 +34,6 @@ abstract class Expr {
         }
         final Expr expression;
     }
-
     static class Literal extends Expr {
         Literal(Object value){
             this.value=value;
@@ -47,7 +44,6 @@ abstract class Expr {
         }
         final Object value;
     }
-
     static class Unary extends Expr {
         Unary(Token operator, Expr right){
             this.operator=operator;
@@ -59,6 +55,16 @@ abstract class Expr {
         }
         final Token operator;
         final Expr right;
+    }
+    static class Variable extends Expr {
+        Variable(Token name){
+            this.name=name;
+        }
+        @Override
+        <R> R accept(Visitor<R> visitor){
+            return visitor.visitVariableExpr(this);
+        }
+        final Token name;
     }
 
     abstract <R> R accept(Visitor<R> visitor);
