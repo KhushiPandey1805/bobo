54f4e74032f2edb34a7c17480bc8fc1859ae4aa7
Khushi Pandey <khushipan1805@gmail.com>
2026-06-25 19:10:54 +0000

    Added assignment and scope

--- Interpreter.java
+++ Interpreter.java
@@ -79,6 +79,22 @@ class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void>{
     private void execute(Stmt stmt){
         stmt.accept(this);
     }
+    void executeBlock(List<Stmt> statements, Environment environment){
+        Environment previous=this.environment;
+        try{
+            this.environment=environment;
+            for(Stmt statement: statements){
+                execute(statement);
+            }
+        } finally {
+            this.environment=previous;
+        }
+    }
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt){
+        executeBlock(stmt.statements,new Environment(environment));
+        return null;
+    }
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt){
         evaluate(stmt.expression);
@@ -98,6 +114,12 @@ class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void>{
         }
         environment.define(stmt.name.lexeme, value);
         return null;
+    }
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr){
+        Object value=evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
     @Override
     public Object visitBinaryExpr(Expr.Binary expr){
