0ba77b4b8f176a1c9cfa2334bfeb5bafcab247f2
Khushi Pandey <khushipan1805@gmail.com>
2026-06-28 19:13:57 +0000

    Added function calls and a clock native function

--- Interpreter.java
+++ Interpreter.java
@@ -1,9 +1,27 @@
 package com.github.khushipandey1805.bobo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void>{
-    private Environment environment=new Environment();
+    final Environment globals=new Environment();
+    private Environment environment=globals;
+    Interpreter(){
+        globals.define("clock", new BoboCallable(){
+            @Override
+            public int arity(){
+                return 0;
+            }
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments){
+                return (double)System.currentTimeMillis()/1000.0;
+            }
+            @Override
+            public String toString(){
+                return "<native fn>";
+            }
+        });
+    }
     void interpret(List<Stmt> statements){
         try{
             for(Stmt statement:statements){
@@ -190,5 +208,21 @@ class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void>{
                 return (double)left*(double)right;
         }
         return null;
+    }
+    @Override
+    public Object visitCallExpr(Expr.Call expr){
+        Object callee=evaluate(expr.callee);
+        List<Object> arguments=new ArrayList<>();
+        for(Expr argument: expr.arguments){
+            arguments.add(evaluate(argument));
+        }
+        if(!(callee instanceof BoboCallable)){
+            throw new RuntimeError(expr.paren, "Bruh, not a function or a class >:(");
+        }
+        BoboCallable function=(BoboCallable)callee;
+        if(arguments.size()!=function.arity()){
+            throw new RuntimeError(expr. paren, "Hey so we wanted "+function.arity()+" arguments but got "+ arguments.size()+" instead soooo...fix that?");
+        }
+        return function.call(this, arguments);
     }
 }
