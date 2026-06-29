d8e3e0ef81bfa4157ea7efd1e808e40901a76392
Khushi Pandey <khushipan1805@gmail.com>
2026-06-29 20:20:22 +0000

    Initial commit

---
+++ BoboFunction.java
@@ -0,0 +1,27 @@
+package com.github.khushipandey1805.bobo;
+
+import java.util.List;
+
+class BoboFunction implements BoboCallable{
+    private final Stmt.Function declaration;
+    BoboFunction(Stmt.Function declaration){
+        this.declaration=declaration;
+    }
+    @Override
+    public String toString(){
+        return "<fn "+declaration.name.lexeme+">";
+    }
+    @Override
+    public int arity(){
+        return declaration.params.size();
+    }
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments){
+        Environment environment=new Environment(interpreter.globals);
+        for(int i=0;i<declaration.params.size();i++){
+            environment.define(declaration.params.get(i).lexeme,arguments.get(i));
+        }
+        interpreter.executeBlock(declaration.body, environment);
+        return null;
+    }
+}
